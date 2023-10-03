ebea0a26132b6b4d6106efb99f4eca4670da8d7d
ladronbx <ladronbravovlc@gmail.com>
2023-10-03 20:45:08 +0200

    fix: Fixed an issue with dark mode in index.html
    - Resolved a bug where the dark mode was not functioning correctly on the index.html page

--- js/main.js
+++ js/main.js
@@ -1,76 +1,12 @@
-
-const nameInput = document.getElementById("name");
-const playButton = document.getElementById("playGameButton");
 const dark = document.getElementById('switch');
 const body = document.body;
-const selectedLevelText = document.getElementById('selectedLevelText');
 
-const storedName = localStorage.getItem("userName");
-const storedLevel = localStorage.getItem("selectedLevel");
-
-function restoreUserData() {
-    if (storedName) {
-        nameInput.value = storedName;
-    }
-
-    if (storedLevel) {
-        selectedLevelText.textContent = `Selected Level: ${getLevelText(storedLevel)}`;
-    }
-}
-
-function saveUserData() {
-    const playerName = nameInput.value.trim();
-    const selectedLevel = localStorage.getItem("selectedLevel");
-
-    if (playerName === "") {
-        alert("Please enter a valid username.");
-    } else if (!selectedLevel) {
-        alert("Please select a level.");
-    } else {
-        localStorage.setItem("userName", playerName);
-        window.location.href = "../pages/colors.html";
-    }
-}
-
-function setupLevelButtons() {
-    const levelButtons = document.querySelectorAll('.level');
-
-    levelButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const selectedLevel = button.getAttribute('data-level');
-            localStorage.setItem('selectedLevel', selectedLevel);
-            selectedLevelText.textContent = `Selected Level: ${getLevelText(selectedLevel)}`;
-        });
-    });
-}
 
 function toggleDarkMode() {
     body.classList.toggle('dark-mode');
     const darkModeEnabled = body.classList.contains('dark-mode');
     localStorage.setItem('dark-mode', darkModeEnabled ? 'enabled' : 'disabled');
 }
-
-function getLevelText(level) {
-    switch (level) {
-        case 'easy':
-            return 'Beginner';
-        case 'medium':
-            return 'Intermediate';
-        case 'advanced':
-            return 'Advanced';
-        default:
-            return '';
-    }
-}
-
-restoreUserData();
-
-playButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    saveUserData();
-});
-
-setupLevelButtons();
 
 dark.addEventListener('click', () => {
     toggleDarkMode();
@@ -79,33 +15,3 @@ dark.addEventListener('click', () => {
 if (localStorage.getItem('dark-mode') === 'enabled') {
     body.classList.add('dark-mode');
 }
-
-
-
-const colorPickers = document.querySelectorAll('.color-picker');
-const playGameButton = document.getElementById('play-game-button');
-const selectedColors = [];
-
-// Cargar colores previamente seleccionados desde el localStorage
-const storedColors = JSON.parse(localStorage.getItem('selectedColors'));
-if (storedColors) {
-    for (let i = 0; i < colorPickers.length; i++) {
-        colorPickers[i].value = storedColors[i];
-        selectedColors.push(storedColors[i]);
-    }
-}
-
-colorPickers.forEach((colorPicker, index) => {
-    colorPicker.addEventListener('input', function () {
-        selectedColors[index] = colorPicker.value;
-    });
-});
-
-playGameButton.addEventListener('click', function () {
-    if (selectedColors.length === 3) {
-        localStorage.setItem('selectedColors', JSON.stringify(selectedColors));
-        window.location.href = "../pages/game.html";
-    } else {
-        alert("Please select exactly three colors.");
-    }
-});
